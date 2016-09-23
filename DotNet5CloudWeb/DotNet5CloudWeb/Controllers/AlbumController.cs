7e2060356a0edb64a3ec8783c2fee8b56ad92092
jerotas <jerotas2005@yahoo.com>
2016-09-23 11:22:57 -0700

    initial commit of non core app

---
+++ DotNet5CloudWeb/DotNet5CloudWeb/Controllers/AlbumController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace DotNet5CloudWeb.Controllers {
+    public class AlbumController : Controller {
+        public ActionResult PopulateDb() {
+            return View();
+        }
+    }
+}
